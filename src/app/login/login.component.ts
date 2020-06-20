9c37d965596deb40c375eb050be01a97a7ac1dab
Emannuel Matos <emannuelloxt@hotmail.com>
2020-06-19 23:50:42 -0400

    :sparkles: feat(login page): check if username only contains letters and numbers

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -6,7 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  username: string = '';
+  password: string = '';
+  isUsernameValid: boolean = true;
+
   constructor() {}
 
   ngOnInit(): void {}
+
+  validateUsername() {
+    const pattern = RegExp(/^[\w-.]*$/);
+    this.isUsernameValid = pattern.test(this.username);
+  }
+
+  onKey(e: any, type: string) {
+    if (type === 'username') {
+      this.username = e.target.value;
+      this.validateUsername();
+    } else if (type === 'password') {
+      this.password = e.target.value;
+    }
+  }
 }
